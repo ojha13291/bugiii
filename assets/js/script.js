1c204ccad85e7b850640ab20941425f36d64021e
Ayush Kumar <57131300+ojha13291@users.noreply.github.com>
2025-01-01 02:57:59 +0530

    Update script.js
    fixed error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 // Smooth Scroll for Navigation
 document.querySelectorAll('a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
     });
-  });
-  
-  // Poem Page Fade-In Effect
-  window.onload = function() {
+});
+
+// Poem Page Fade-In Effect
+window.onload = function() {
     const poems = document.querySelectorAll('main p');
     poems.forEach((poem, index) => {
-      setTimeout(() => {
-        poem.style.opacity = 1;
-        poem.style.transition = 'opacity 2s ease';
-      }, index * 1000);
+        setTimeout(() => {
+            poem.style.opacity = 1;
+            poem.style.transition = 'opacity 2s ease';
+        }, index * 1000);
     });
-  };
-  
+};
